f52cdd77669538ad3b59be8049899abbc440ea1b
nrubin29 <nrubin29@gmail.com>
2020-05-29 03:09:29 -0400

    Refactored profile view into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'lastfm.dart';
-import 'types/luser.dart';
+import 'package:simplescrobble/views/profile_view.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,61 +8,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'simplescrobble',
-        home: FutureBuilder<LUser>(
-          future: Lastfm().getUser('nrubin29'),
-          builder: (context, snapshot) {
-            final user = snapshot.data;
-
-            if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            } else if (!snapshot.hasData) {
-              return CircularProgressIndicator();
-            }
-
-            return Scaffold(
-              appBar: AppBar(
-                title:
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  CircleAvatar(
-                      backgroundImage: NetworkImage(user.images.last.url)),
-                  SizedBox(width: 8),
-                  Text(user.name)
-                ]),
-              ),
-              body: Center(
-                  child: Column(
-                children: [
-                  SizedBox(height: 10),
-                  Text('Scrobbling since ${user.registered.dateFormatted}'),
-                  SizedBox(height: 10),
-                  IntrinsicHeight(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        children: [
-                          Text('Scrobbles'),
-                          Text(user.playCountFormatted)
-                        ],
-                      ),
-                      VerticalDivider(),
-                      Column(
-                        children: [Text('Artists'), Text('???')],
-                      ),
-                      VerticalDivider(),
-                      Column(
-                        children: [Text('Albums'), Text('???')],
-                      ),
-                      VerticalDivider(),
-                      Column(
-                        children: [Text('Tracks'), Text('???')],
-                      ),
-                    ],
-                  ))
-                ],
-              )),
-            );
-          },
-        ));
+        theme:
+            ThemeData.from(colorScheme: ColorScheme.light(primary: Colors.red)),
+        home: ProfileView(username: 'nrubin29'));
   }
 }
---
+++ lib/views/profile_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:simplescrobble/lastfm.dart';
+import 'package:simplescrobble/types/luser.dart';
+
+class ProfileView extends StatelessWidget {
+  String username;
+
+  ProfileView({Key key, this.username}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<LUser>(
+      future: Lastfm().getUser(username),
+      builder: (context, snapshot) {
+        final user = snapshot.data;
+
+        if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        } else if (!snapshot.hasData) {
+          return CircularProgressIndicator();
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              CircleAvatar(backgroundImage: NetworkImage(user.images.last.url)),
+              SizedBox(width: 8),
+              Text(user.name)
+            ]),
+          ),
+          body: Center(
+              child: Column(
+            children: [
+              SizedBox(height: 10),
+              Text('Scrobbling since ${user.registered.dateFormatted}'),
+              SizedBox(height: 10),
+              IntrinsicHeight(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Text('Scrobbles'),
+                      Text(user.playCountFormatted)
+                    ],
+                  ),
+                  VerticalDivider(),
+                  Column(
+                    children: [Text('Artists'), Text('???')],
+                  ),
+                  VerticalDivider(),
+                  Column(
+                    children: [Text('Albums'), Text('???')],
+                  ),
+                  VerticalDivider(),
+                  Column(
+                    children: [Text('Tracks'), Text('???')],
+                  ),
+                ],
+              ))
+            ],
+          )),
+        );
+      },
+    );
+  }
+}
