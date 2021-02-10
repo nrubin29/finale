2d070271cc3828f2f96d81f8f070306b1af65535
nrubin29 <nrubin29@gmail.com>
2021-02-09 22:59:45 -0500

    Weekly chart view shows top three tracks, albums, and artists

--- lib/views/weekly_chart_view.dart
+++ lib/views/weekly_chart_view.dart
@@ -54,159 +54,146 @@ class _WeeklyChartViewState extends State<WeeklyChartView> {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: Text(widget.chart.displayTitle)),
-        body: Builder(
-          builder: (context) => !_loaded
-              ? LoadingComponent()
-              : Column(
-                  children: [
-                    SizedBox(height: 10),
-                    Text(
-                      '$_numScrobbles scrobbles',
-                      style: TextStyle(fontSize: 18),
+        body: !_loaded
+            ? LoadingComponent()
+            : ListView(
+                children: [
+                  SizedBox(height: 10),
+                  Text(
+                    '$_numScrobbles scrobbles',
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 10),
+                  if (_tracks.isNotEmpty)
+                    Container(
+                      margin: EdgeInsets.only(left: 8),
+                      child: Text('Top Tracks'),
                     ),
-                    SizedBox(height: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        if (_tracks.isNotEmpty)
-                          Container(
-                            margin: EdgeInsets.only(left: 8),
-                            child: Text('Top Track'),
+                  for (final track in _tracks.take(3))
+                    ListTile(
+                      title: Text(track.name),
+                      subtitle: Text(track.artist),
+                      trailing: Text('${track.playCount} scrobbles'),
+                      leading: ImageComponent(displayable: track),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TrackView(track: track),
                           ),
-                        if (_tracks.isNotEmpty)
-                          ListTile(
-                            title: Text(_tracks.first.name),
-                            subtitle: Text(_tracks.first.artist),
-                            trailing:
-                                Text('${_tracks.first.playCount} scrobbles'),
-                            leading: ImageComponent(displayable: _tracks.first),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      TrackView(track: _tracks.first),
-                                ),
-                              );
-                            },
-                          ),
-                        if (_tracks.isNotEmpty)
-                          ListTile(
-                            title: Text('See more'),
-                            trailing: Icon(Icons.chevron_right),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Scaffold(
-                                    appBar: AppBar(title: Text('Top Tracks')),
-                                    body: DisplayComponent<
-                                        LUserWeeklyTrackChartTrack>(
-                                      items: _tracks,
-                                      detailWidgetProvider: (track) =>
-                                          TrackView(track: track),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        if (_tracks.isNotEmpty) Divider(),
-                        if (_albums.isNotEmpty)
-                          Container(
-                            margin: EdgeInsets.only(left: 8),
-                            child: Text('Top Album'),
-                          ),
-                        if (_albums.isNotEmpty)
-                          ListTile(
-                            title: Text(_albums.first.name),
-                            subtitle: Text(_albums.first.artist.name),
-                            trailing:
-                                Text('${_albums.first.playCount} scrobbles'),
-                            leading: ImageComponent(displayable: _albums.first),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      AlbumView(album: _albums.first),
-                                ),
-                              );
-                            },
-                          ),
-                        if (_albums.isNotEmpty)
-                          ListTile(
-                            title: Text('See more'),
-                            trailing: Icon(Icons.chevron_right),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Scaffold(
-                                    appBar: AppBar(title: Text('Top Albums')),
-                                    body: DisplayComponent<
-                                        LUserWeeklyAlbumChartAlbum>(
-                                      items: _albums,
-                                      displayType: DisplayType.grid,
-                                      detailWidgetProvider: (album) =>
-                                          AlbumView(album: album),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        if (_albums.isNotEmpty) Divider(),
-                        if (_artists.isNotEmpty)
-                          Container(
-                            margin: EdgeInsets.only(left: 8),
-                            child: Text('Top Artist'),
-                          ),
-                        if (_artists.isNotEmpty)
-                          ListTile(
-                            title: Text(_artists.first.name),
-                            trailing:
-                                Text('${_artists.first.playCount} scrobbles'),
-                            leading:
-                                ImageComponent(displayable: _artists.first),
-                            contentPadding: EdgeInsets.symmetric(
-                                vertical: 8, horizontal: 16),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      ArtistView(artist: _artists.first),
-                                ),
-                              );
-                            },
-                          ),
-                        if (_artists.isNotEmpty)
-                          ListTile(
-                            title: Text('See more'),
-                            trailing: Icon(Icons.chevron_right),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Scaffold(
-                                    appBar: AppBar(title: Text('Top Artists')),
-                                    body: DisplayComponent<
-                                        LUserWeeklyArtistChartArtist>(
-                                      items: _artists,
-                                      displayType: DisplayType.grid,
-                                      detailWidgetProvider: (artist) =>
-                                          ArtistView(artist: artist),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                      ],
+                        );
+                      },
                     ),
-                  ],
-                ),
-        ),
+                  if (_tracks.length > 3)
+                    ListTile(
+                      title: Text('See more'),
+                      trailing: Icon(Icons.chevron_right),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              appBar: AppBar(title: Text('Top Tracks')),
+                              body:
+                                  DisplayComponent<LUserWeeklyTrackChartTrack>(
+                                items: _tracks,
+                                detailWidgetProvider: (track) =>
+                                    TrackView(track: track),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  if (_tracks.isNotEmpty) Divider(),
+                  if (_albums.isNotEmpty)
+                    Container(
+                      margin: EdgeInsets.only(left: 8),
+                      child: Text('Top Albums'),
+                    ),
+                  for (final album in _albums.take(3))
+                    ListTile(
+                      title: Text(album.name),
+                      subtitle: Text(album.artist.name),
+                      trailing: Text('${album.playCount} scrobbles'),
+                      leading: ImageComponent(displayable: album),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AlbumView(album: album),
+                          ),
+                        );
+                      },
+                    ),
+                  if (_albums.length > 3)
+                    ListTile(
+                      title: Text('See more'),
+                      trailing: Icon(Icons.chevron_right),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              appBar: AppBar(title: Text('Top Albums')),
+                              body:
+                                  DisplayComponent<LUserWeeklyAlbumChartAlbum>(
+                                items: _albums,
+                                displayType: DisplayType.grid,
+                                detailWidgetProvider: (album) =>
+                                    AlbumView(album: album),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  if (_albums.isNotEmpty) Divider(),
+                  if (_artists.isNotEmpty)
+                    Container(
+                      margin: EdgeInsets.only(left: 8),
+                      child: Text('Top Artists'),
+                    ),
+                  for (final artist in _artists.take(3))
+                    ListTile(
+                      title: Text(artist.name),
+                      trailing: Text('${artist.playCount} scrobbles'),
+                      leading: ImageComponent(displayable: artist),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ArtistView(artist: artist),
+                          ),
+                        );
+                      },
+                    ),
+                  if (_artists.length > 3)
+                    ListTile(
+                      title: Text('See more'),
+                      trailing: Icon(Icons.chevron_right),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              appBar: AppBar(title: Text('Top Artists')),
+                              body: DisplayComponent<
+                                  LUserWeeklyArtistChartArtist>(
+                                items: _artists,
+                                displayType: DisplayType.grid,
+                                detailWidgetProvider: (artist) =>
+                                    ArtistView(artist: artist),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                ],
+              ),
       );
 }
