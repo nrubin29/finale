ad4e3cd8c664f0e117fd38cf24f90bad6bc5d42e
nrubin29 <nrubin29@gmail.com>
2021-08-07 17:00:17 -0400

    Added some padding to the loading component

--- lib/widgets/base/loading_component.dart
+++ lib/widgets/base/loading_component.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class LoadingComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Container(
-      color: Theme.of(context).backgroundColor,
-      child: Center(child: CircularProgressIndicator()));
+        color: Theme.of(context).backgroundColor,
+        child: const Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      );
 }
