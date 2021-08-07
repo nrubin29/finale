82efb5c0d79fc0791d79d7829411ed1af8c81991
nrubin29 <nrubin29@gmail.com>
2021-08-07 17:41:22 -0400

    Added safe area and scrolling to batch scrobble view

--- lib/widgets/scrobble/batch_scrobble_view.dart
+++ lib/widgets/scrobble/batch_scrobble_view.dart
@@ -2,6 +2,7 @@ import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:finale/services/generic.dart';
 import 'package:finale/services/lastfm/lastfm.dart';
 import 'package:finale/util/util.dart';
+import 'package:finale/widgets/base/app_bar.dart';
 import 'package:finale/widgets/entity/entity_image.dart';
 import 'package:flutter/material.dart';
 
@@ -66,131 +67,135 @@ class _BatchScrobbleViewState extends State<BatchScrobbleView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Scrobble'),
-          actions: [
-            Builder(
-                builder: (context) => IconButton(
-                    icon: Icon(scrobbleIcon),
-                    onPressed: () => _scrobble(context)))
-          ],
+      appBar: createAppBar(
+        'Scrobble',
+        actions: [
+          Builder(
+              builder: (context) => IconButton(
+                  icon: Icon(scrobbleIcon),
+                  onPressed: () => _scrobble(context)))
+        ],
+      ),
+      body: SafeArea(
+        child: Form(
+          child: ListView(
+            padding: const EdgeInsets.all(10),
+            physics: const ScrollPhysics(),
+            children: [
+              ListTile(
+                leading: EntityImage(entity: widget.entity),
+                title: Text(widget.entity.displayTitle),
+                subtitle: widget.entity.displaySubtitle != null
+                    ? Text(widget.entity.displaySubtitle!)
+                    : null,
+                trailing:
+                    Text(formatScrobbles(widget.entity.tracks.length, 'track')),
+              ),
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Scrobble behavior',
+                  style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                      color: Theme.of(context).textTheme.caption!.color),
+                ),
+              ),
+              RadioListTile<ScrobbleTimestampBehavior>(
+                activeColor: Colors.red,
+                value: ScrobbleTimestampBehavior.startingNow,
+                groupValue: _behavior,
+                onChanged: (value) {
+                  if (value != null) {
+                    setState(() {
+                      _behavior = value;
+                    });
+                  }
+                },
+                title: Text('Starting now'),
+              ),
+              RadioListTile<ScrobbleTimestampBehavior>(
+                activeColor: Colors.red,
+                value: ScrobbleTimestampBehavior.startingCustom,
+                groupValue: _behavior,
+                onChanged: (value) {
+                  if (value != null) {
+                    setState(() {
+                      _behavior = value;
+                      _customTimestamp = DateTime.now();
+                    });
+                  }
+                },
+                title: Text('Starting at a custom timestamp'),
+              ),
+              RadioListTile<ScrobbleTimestampBehavior>(
+                activeColor: Colors.red,
+                value: ScrobbleTimestampBehavior.endingNow,
+                groupValue: _behavior,
+                onChanged: (value) {
+                  if (value != null) {
+                    setState(() {
+                      _behavior = value;
+                    });
+                  }
+                },
+                title: Text('Ending now'),
+              ),
+              RadioListTile<ScrobbleTimestampBehavior>(
+                activeColor: Colors.red,
+                value: ScrobbleTimestampBehavior.endingCustom,
+                groupValue: _behavior,
+                onChanged: (value) {
+                  if (value != null) {
+                    setState(() {
+                      _behavior = value;
+                      _customTimestamp = DateTime.now();
+                    });
+                  }
+                },
+                title: Text('Ending at a custom timestamp'),
+              ),
+              Visibility(
+                visible:
+                    _behavior == ScrobbleTimestampBehavior.startingCustom ||
+                        _behavior == ScrobbleTimestampBehavior.endingCustom,
+                child: DateTimeField(
+                  decoration: InputDecoration(labelText: 'Timestamp'),
+                  resetIcon: null,
+                  format: dateTimeFormatWithYear,
+                  initialValue: _customTimestamp,
+                  onShowPicker: (context, currentValue) async {
+                    final date = await showDatePicker(
+                        context: context,
+                        initialDate: currentValue ?? DateTime.now(),
+                        firstDate: DateTime.now().subtract(Duration(days: 14)),
+                        lastDate: DateTime.now().add(Duration(days: 1)));
+
+                    if (date != null) {
+                      final time = await showTimePicker(
+                          context: context,
+                          initialTime: TimeOfDay.fromDateTime(
+                              currentValue ?? DateTime.now()));
+
+                      if (time != null) {
+                        return DateTimeField.combine(date, time);
+                      }
+                    }
+
+                    return currentValue;
+                  },
+                  onChanged: (dateTime) {
+                    if (dateTime != null) {
+                      setState(() {
+                        _customTimestamp = dateTime;
+                      });
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
-        body: Form(
-            child: Container(
-                margin: EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: EntityImage(entity: widget.entity),
-                      title: Text(widget.entity.displayTitle),
-                      subtitle: widget.entity.displaySubtitle != null
-                          ? Text(widget.entity.displaySubtitle!)
-                          : null,
-                      trailing: Text(formatScrobbles(
-                          widget.entity.tracks.length, 'track')),
-                    ),
-                    SizedBox(height: 10),
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text('Scrobble behavior',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText2!
-                                .copyWith(
-                                    color: Theme.of(context)
-                                        .textTheme
-                                        .caption!
-                                        .color))),
-                    RadioListTile<ScrobbleTimestampBehavior>(
-                        activeColor: Colors.red,
-                        value: ScrobbleTimestampBehavior.startingNow,
-                        groupValue: _behavior,
-                        onChanged: (value) {
-                          if (value != null) {
-                            setState(() {
-                              _behavior = value;
-                            });
-                          }
-                        },
-                        title: Text('Starting now')),
-                    RadioListTile<ScrobbleTimestampBehavior>(
-                        activeColor: Colors.red,
-                        value: ScrobbleTimestampBehavior.startingCustom,
-                        groupValue: _behavior,
-                        onChanged: (value) {
-                          if (value != null) {
-                            setState(() {
-                              _behavior = value;
-                              _customTimestamp = DateTime.now();
-                            });
-                          }
-                        },
-                        title: Text('Starting at a custom timestamp')),
-                    RadioListTile<ScrobbleTimestampBehavior>(
-                        activeColor: Colors.red,
-                        value: ScrobbleTimestampBehavior.endingNow,
-                        groupValue: _behavior,
-                        onChanged: (value) {
-                          if (value != null) {
-                            setState(() {
-                              _behavior = value;
-                            });
-                          }
-                        },
-                        title: Text('Ending now')),
-                    RadioListTile<ScrobbleTimestampBehavior>(
-                        activeColor: Colors.red,
-                        value: ScrobbleTimestampBehavior.endingCustom,
-                        groupValue: _behavior,
-                        onChanged: (value) {
-                          if (value != null) {
-                            setState(() {
-                              _behavior = value;
-                              _customTimestamp = DateTime.now();
-                            });
-                          }
-                        },
-                        title: Text('Ending at a custom timestamp')),
-                    Visibility(
-                      visible: _behavior ==
-                              ScrobbleTimestampBehavior.startingCustom ||
-                          _behavior == ScrobbleTimestampBehavior.endingCustom,
-                      child: DateTimeField(
-                          decoration: InputDecoration(labelText: 'Timestamp'),
-                          resetIcon: null,
-                          format: dateTimeFormatWithYear,
-                          initialValue: _customTimestamp,
-                          onShowPicker: (context, currentValue) async {
-                            final date = await showDatePicker(
-                                context: context,
-                                initialDate: currentValue ?? DateTime.now(),
-                                firstDate:
-                                    DateTime.now().subtract(Duration(days: 14)),
-                                lastDate:
-                                    DateTime.now().add(Duration(days: 1)));
-
-                            if (date != null) {
-                              final time = await showTimePicker(
-                                  context: context,
-                                  initialTime: TimeOfDay.fromDateTime(
-                                      currentValue ?? DateTime.now()));
-
-                              if (time != null) {
-                                return DateTimeField.combine(date, time);
-                              }
-                            }
-
-                            return currentValue;
-                          },
-                          onChanged: (dateTime) {
-                            if (dateTime != null) {
-                              setState(() {
-                                _customTimestamp = dateTime;
-                              });
-                            }
-                          }),
-                    ),
-                  ],
-                ))));
+      ),
+    );
   }
 }
