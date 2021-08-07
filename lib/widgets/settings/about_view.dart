7e32757dd0f1b697bfbc6e6e6e544f102e5e3b59
nrubin29 <nrubin29@gmail.com>
2021-08-07 19:10:05 -0400

    Added safe area and scrolling to about view

--- lib/widgets/settings/about_view.dart
+++ lib/widgets/settings/about_view.dart
@@ -1,4 +1,5 @@
 import 'package:finale/util/util.dart';
+import 'package:finale/widgets/base/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:social_media_buttons/social_media_icons.dart';
@@ -8,99 +9,125 @@ class AboutView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('About')),
-        body: Builder(
-            builder: (context) => Center(
-                    child: SafeArea(
-                        child: Column(
+      appBar: createAppBar('About'),
+      body: CustomScrollView(
+        physics: const ScrollPhysics(),
+        slivers: [
+          SliverToBoxAdapter(child: SizedBox(height: 20)),
+          SliverToBoxAdapter(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                appIcon(size: 84),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        appIcon(size: 84),
-                        SizedBox(width: 10),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Finale',
-                              style: TextStyle(fontSize: 24),
-                            ),
-                            FutureBuilder<PackageInfo>(
-                                future: PackageInfo.fromPlatform(),
-                                builder: (context, snapshot) => snapshot.hasData
-                                    ? Text(
-                                        'Version ${snapshot.data!.fullVersion}')
-                                    : SizedBox())
-                          ],
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    RichText(
-                        text: TextSpan(
-                            style: Theme.of(context).textTheme.bodyText2,
-                            children: [
-                          TextSpan(text: 'Made with '),
-                          WidgetSpan(
-                              child: Icon(
-                            Icons.favorite,
-                            size: 16,
-                          )),
-                          TextSpan(text: ' and '),
-                          WidgetSpan(
-                              child: Icon(
-                            Icons.music_note,
-                            size: 16,
-                          )),
-                          TextSpan(text: ' by Noah Rubin')
-                        ])),
-                    SizedBox(height: 10),
-                    ListTile(
-                      title: Text('Follow me on Twitter'),
-                      leading: Icon(SocialMediaIcons.twitter),
-                      trailing: Icon(Icons.chevron_right),
-                      onTap: () {
-                        launch('https://twitter.com/nrubin29');
-                      },
-                    ),
-                    ListTile(
-                      title: Text('My website'),
-                      leading: Icon(Icons.web),
-                      trailing: Icon(Icons.chevron_right),
-                      onTap: () {
-                        launch('https://nrubintech.com');
-                      },
-                    ),
-                    ListTile(
-                        title: Text('Source code'),
-                        leading: Icon(SocialMediaIcons.github_circled),
-                        trailing: Icon(Icons.chevron_right),
-                        onTap: () {
-                          launch('https://github.com/nrubin29/finale');
-                        }),
-                    ListTile(
-                      title: Text('Send feedback'),
-                      leading: Icon(Icons.email),
-                      trailing: Icon(Icons.chevron_right),
-                      onTap: () {
-                        launch('mailto:feedback@finale.app'
-                            '?subject=Finale%20feedback');
-                      },
-                    ),
-                    ListTile(
-                      title: Text('Privacy policy'),
-                      leading: Icon(Icons.privacy_tip),
-                      trailing: Icon(Icons.chevron_right),
-                      onTap: () {
-                        launch('https://finale.app/privacy');
-                      },
-                    ),
-                    Spacer(),
-                    Text('\u00a9 2021 Noah Rubin Technologies LLC'),
-                    Text('All rights reserved'),
+                    Text('Finale', style: TextStyle(fontSize: 24)),
+                    FutureBuilder<PackageInfo>(
+                      future: PackageInfo.fromPlatform(),
+                      builder: (_, snapshot) => snapshot.hasData
+                          ? Text('Version ${snapshot.data!.fullVersion}')
+                          : SizedBox(),
+                    )
                   ],
-                )))));
+                )
+              ],
+            ),
+          ),
+          SliverToBoxAdapter(child: SizedBox(height: 20)),
+          SliverToBoxAdapter(
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                style: Theme.of(context).textTheme.bodyText2,
+                children: [
+                  TextSpan(text: 'Made with '),
+                  WidgetSpan(
+                    child: Icon(
+                      Icons.favorite,
+                      size: 16,
+                    ),
+                  ),
+                  TextSpan(text: ' and '),
+                  WidgetSpan(
+                    child: Icon(
+                      Icons.music_note,
+                      size: 16,
+                    ),
+                  ),
+                  TextSpan(text: ' by Noah Rubin'),
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(child: SizedBox(height: 10)),
+          SliverToBoxAdapter(
+            child: ListTile(
+              title: Text('Follow me on Twitter'),
+              leading: Icon(SocialMediaIcons.twitter),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () {
+                launch('https://twitter.com/nrubin29');
+              },
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ListTile(
+              title: Text('My website'),
+              leading: Icon(Icons.web),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () {
+                launch('https://nrubintech.com');
+              },
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ListTile(
+              title: Text('Source code'),
+              leading: Icon(SocialMediaIcons.github_circled),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () {
+                launch('https://github.com/nrubin29/finale');
+              },
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ListTile(
+              title: Text('Send feedback'),
+              leading: Icon(Icons.email),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () {
+                launch('mailto:feedback@finale.app'
+                    '?subject=Finale%20feedback');
+              },
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ListTile(
+              title: Text('Privacy policy'),
+              leading: Icon(Icons.privacy_tip),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () {
+                launch('https://finale.app/privacy');
+              },
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: SafeArea(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('\u00a9 2021 Noah Rubin Technologies LLC'),
+                  Text('All rights reserved'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
