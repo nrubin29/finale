958af45d37df3ebe517c67203f20b08ddf044faf
nrubin29 <nrubin29@gmail.com>
2022-02-23 18:47:28 -0800

    Only show the Log In with Strava button when the user isn't logged in

--- lib/widgets/tools/workout_view.dart
+++ lib/widgets/tools/workout_view.dart
@@ -1,5 +1,6 @@
 import 'package:finale/services/strava/activity.dart';
 import 'package:finale/services/strava/strava.dart';
+import 'package:finale/util/preferences.dart';
 import 'package:finale/util/util.dart';
 import 'package:finale/widgets/base/app_bar.dart';
 import 'package:finale/widgets/tools/workout_details.dart';
@@ -15,10 +16,24 @@ class WorkoutView extends StatefulWidget {
 class _WorkoutViewState extends State<WorkoutView> {
   List<AthleteActivity>? _activities;
 
+  @override
+  void initState() {
+    super.initState();
+    _fetchActivities();
+  }
+
   Future<void> _authenticate() async {
     await Strava().authenticate();
+    await _fetchActivities();
+  }
 
-    var activities = await const StravaListActivitiesRequest().doRequest(20, 1);
+  Future<void> _fetchActivities() async {
+    if (!Preferences().hasStravaAuthData) {
+      return;
+    }
+
+    final activities =
+        await const StravaListActivitiesRequest().doRequest(20, 1);
     setState(() {
       _activities = activities;
     });
@@ -27,30 +42,35 @@ class _WorkoutViewState extends State<WorkoutView> {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: createAppBar('Strava Workouts'),
-        body: ListView(
-          children: [
-            TextButton(
-              onPressed: _authenticate,
-              child: const Text('Log in with Strava'),
-            ),
-            if (_activities != null)
-              for (final activity in _activities!)
-                ListTile(
-                  title: Text(activity.name),
-                  subtitle: Text(dateTimeFormat.format(activity.startDateLocal) +
-                      ' - ' +
-                      timeFormat.format(activity.endDateLocal)),
-                  leading: const Icon(Icons.run_circle),
-                  trailing: const Icon(Icons.chevron_right),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => WorkoutDetails(activity: activity)),
-                    );
-                  },
+        body: Preferences().hasStravaAuthData
+            ? ListView(
+                children: [
+                  if (_activities != null)
+                    for (final activity in _activities!)
+                      ListTile(
+                        title: Text(activity.name),
+                        subtitle: Text(
+                            dateTimeFormat.format(activity.startDateLocal) +
+                                ' - ' +
+                                timeFormat.format(activity.endDateLocal)),
+                        leading: const Icon(Icons.run_circle),
+                        trailing: const Icon(Icons.chevron_right),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) =>
+                                    WorkoutDetails(activity: activity)),
+                          );
+                        },
+                      ),
+                ],
+              )
+            : Center(
+                child: TextButton(
+                  onPressed: _authenticate,
+                  child: const Text('Log in with Strava'),
                 ),
-          ],
-        ),
+              ),
       );
 }
