a0e4248ed5626d557478224e31a6e54883c4f109
nrubin29 <nrubin29@gmail.com>
2024-12-09 18:05:01 -0800

    Add ACRCloud error message when no sound is detected

--- lib/services/acrcloud/acrcloud.dart
+++ lib/services/acrcloud/acrcloud.dart
@@ -2,17 +2,25 @@ import 'package:flutter_acrcloud/flutter_acrcloud.dart';
 
 const _okErrorCodes = {/* Success: */ 0, /* No results: */ 1001};
 const _limitExceededErrorCodes = {/* Request count: */ 3003, /* QpS: */ 3015};
+const _noFingerprintCode = 2004;
 
 extension ACRCloudResponseError on ACRCloudResponse {
   String? get errorMessage {
     final statusCode = status.code;
-    if (!_okErrorCodes.contains(statusCode)) {
-      return _limitExceededErrorCodes.contains(statusCode)
-          ? 'Too many people are using Finale right now. Please try again '
-              'later.'
-          : status.msg;
+
+    if (_okErrorCodes.contains(statusCode)) {
+      return null;
     }
 
-    return null;
+    if (_limitExceededErrorCodes.contains(statusCode)) {
+      return 'Too many people are using Finale right now. Please try again '
+          'later.';
+    }
+
+    if (statusCode == _noFingerprintCode) {
+      return 'No sound detected.';
+    }
+
+    return status.msg;
   }
 }
