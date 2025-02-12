cc71d00619132f75a011b52e04047f0a30c3c043
nrubin29 <nrubin29@gmail.com>
2025-02-11 20:53:39 -0800

    Improve the performance of PeriodPagedRequest for custom periods by implementing a local cache

--- lib/services/lastfm/period_paged_request.dart
+++ lib/services/lastfm/period_paged_request.dart
@@ -25,12 +25,21 @@ class _CacheKey {
   int get hashCode => Object.hash(username, period);
 }
 
-final _cache = <_CacheKey, List<LRecentTracksResponseTrack>>{};
+/// A global cache of (username, period) -> tracks.
+///
+/// This cache can be global because all custom requests are based on tracks.
+final _globalCache = <_CacheKey, List<LRecentTracksResponseTrack>>{};
 
 abstract class PeriodPagedRequest<R extends LPagedResponse<T>,
     T extends HasPlayCount> extends PagedRequest<T> {
   final String username;
   final Period? period;
+
+  /// A local cache of post-processed data.
+  ///
+  /// This is useful for speeding up subsequent page loads as [map], which can
+  /// do async work, only needs to be called once per track.
+  List<T>? _localCachedData;
 
   PeriodPagedRequest(this.username, this.period);
 
@@ -40,52 +49,67 @@ abstract class PeriodPagedRequest<R extends LPagedResponse<T>,
 
   Future<T> map(MapEntry<String, List<LRecentTracksResponseTrack>> entry);
 
+  /// Populates [_globalCache] and [_localCachedData] for this request, as
+  /// needed.
+  Future<void> _populateCachedData(Period period) async {
+    List<LRecentTracksResponseTrack> response;
+    final cacheKey = _CacheKey(username, period);
+    final globalCachedData = _globalCache[cacheKey];
+
+    if (globalCachedData != null) {
+      response = globalCachedData;
+    } else {
+      final request = GetRecentTracksRequest(username,
+          from: period.start, to: period.end, extended: true);
+
+      response = await request.getAllData();
+      _globalCache[cacheKey] = response;
+    }
+
+    final groupedData = response.groupListsBy(groupBy);
+    // If an entity doesn't have a name, it will be grouped into the empty
+    // string group which will mess up the request. The best we can do is
+    // filter it out.
+    groupedData.remove('');
+    _localCachedData = (await Future.wait(groupedData.entries.map(map)))
+        .sorted((a, b) => b.playCount.compareTo(a.playCount));
+  }
+
   @override
   @nonVirtual
   doRequest(int limit, int page) async {
     final period = this.period ?? Preferences.period.value;
 
-    if (period.isCustom) {
-      List<LRecentTracksResponseTrack> response;
-      final cacheKey = _CacheKey(username, period);
-      final cachedData = _cache[cacheKey];
-
-      if (cachedData != null) {
-        response = cachedData;
-      } else {
-        final request = GetRecentTracksRequest(username,
-            from: period.start, to: period.end, extended: true);
-
-        response = await request.getAllData();
-        _cache[cacheKey] = response;
-      }
-
-      final groupedData = response.groupListsBy(groupBy);
-      // If an entity doesn't have a name, it will be grouped into the empty
-      // string group which will mess up the request. The best we can do is
-      // filter it out.
-      groupedData.remove('');
-      final data = (await Future.wait(groupedData.entries.map(map)))
-          .sorted((a, b) => b.playCount.compareTo(a.playCount));
-
-      return data.slice(limit * (page - 1), min(limit * page, data.length));
+    if (!period.isCustom) {
+      return (await doPeriodRequest(period, limit, page)).items;
     }
 
-    return (await doPeriodRequest(period, limit, page)).items;
+    // If page 1 is being requested, either it's the first request and
+    // [_cachedData] is already null or we probably want fresh data.
+    if (page == 1) {
+      _localCachedData = null;
+    }
+
+    if (_localCachedData == null) {
+      await _populateCachedData(period);
+    }
+
+    return _localCachedData!
+        .slice(limit * (page - 1), min(limit * page, _localCachedData!.length));
   }
 
   @nonVirtual
   Future<int> getNumItems() async {
     final period = this.period ?? Preferences.period.value;
 
-    if (period.isCustom) {
-      return (await getAllData()).length;
+    if (!period.isCustom) {
+      return (await doPeriodRequest(period, 1, 1)).attr.total;
     }
 
-    return (await doPeriodRequest(period, 1, 1)).attr.total;
+    return _localCachedData?.length ?? (await getAllData()).length;
   }
 
   @override
   String toString() => '$runtimeType(user=$username, '
-      'period=${(period ?? Preferences.period.value).value})';
+      'period=${period ?? Preferences.period.value})';
 }
