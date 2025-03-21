b5d493413961aee2356771de1d9a3d31ab59fe43
nrubin29 <nrubin29@gmail.com>
2025-03-21 14:46:09 -0700

    Fix ListTileTextField title color

--- lib/widgets/base/list_tile_text_field.dart
+++ lib/widgets/base/list_tile_text_field.dart
@@ -12,22 +12,30 @@ class ListTileTextField extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => SafeArea(
-    top: false,
-    bottom: false,
-    minimum: const EdgeInsets.symmetric(horizontal: 16),
-    child: Row(
-      children: [
-        Text(title, style: Theme.of(context).textTheme.titleMedium),
-        const SizedBox(width: 16),
-        Expanded(
-          child: TextFormField(
-            controller: controller,
-            textAlign: TextAlign.end,
-            validator: validator,
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SafeArea(
+      top: false,
+      bottom: false,
+      minimum: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        children: [
+          Text(
+            title,
+            style: theme.textTheme.bodyLarge?.copyWith(
+              color: theme.colorScheme.onSurface,
+            ),
           ),
-        ),
-      ],
-    ),
-  );
+          const SizedBox(width: 16),
+          Expanded(
+            child: TextFormField(
+              controller: controller,
+              textAlign: TextAlign.end,
+              validator: validator,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
